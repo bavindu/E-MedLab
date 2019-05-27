76de74cd99f6c6e1b22f27add8264f2ff3d8339d
Bavindu <bavindubimsara.16@cse.mrt.ac.lk>
2019-05-27 23:45:12 +0530

    change mongo uri

--- models/metaModels/db.js
+++ models/metaModels/db.js
@@ -1,17 +1,25 @@
 const mongoose=require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-let db = 'MedicalLab';
-
-if(process.env.NODE_ENV!=='test'){
-    mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true},(err)=>{
-        if(!err){
-            console.log('connected to '+process.env.MONGODB_URI)
-        }
-        else{
-            console.log("error connecting to mongodb "+err);
-        }
-    });
-}
+// let db = 'MedicalLab';
+//
+// if(process.env.NODE_ENV!=='test'){
+//     mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true},(err)=>{
+//         if(!err){
+//             console.log('connected to '+process.env.MONGODB_URI)
+//         }
+//         else{
+//             console.log("error connecting to mongodb "+err);
+//         }
+//     });
+// }
+mongoose.connect("mongodb+srv://bavindu:metalica@1996@cluster0-lbiru.mongodb.net/test?retryWrites=true",{useNewUrlParser:true},(err)=>{
+    if(!err){
+        console.log('connected to '+process.env.MONGODB_URI)
+    }
+    else{
+        console.log("error connecting to mongodb "+err);
+    }
+});
 
 
